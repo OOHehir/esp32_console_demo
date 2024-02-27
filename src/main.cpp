220a2d4eaafb3686b4273b2c63cac875cccf19d8
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-02-27 19:40:05 +0000

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+#include "esp_log.h"
 #include "esp_spi_flash.h"
+#include "esp_console.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 #include "driver/gpio.h"
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
@@ -24,7 +28,25 @@
 #define CS_PIN SS
 #define LED_PIN GPIO_NUM_8
 
+static const char *TAG = "main";
+#define PROMPT_STR CONFIG_IDF_TARGET
+
 TaskHandle_t flashLED_TaskHandler = nullptr;
+
+#ifndef CONFIG_ESP_CONSOLE_UART_DEFAULT
+#error "CONFIG_ESP_CONSOLE_UART_DEFAULT not defined in sdkconfig"
+#endif
+
+/*
+ * We warn if a secondary serial console is enabled. A secondary serial console is always output-only and
+ * hence not very useful for interactive console applications. If you encounter this warning, consider disabling
+ * the secondary serial console in menuconfig unless you know what you are doing.
+ */
+#if SOC_USB_SERIAL_JTAG_SUPPORTED
+#if !CONFIG_ESP_CONSOLE_SECONDARY_NONE
+#warning "A secondary serial console is not useful when using the console component. Please disable it in menuconfig."
+#endif
+#endif
 
 // Setup for software SPI:
 // #define DATA_PIN 2
@@ -51,11 +73,20 @@ extern "C"
     void app_main(void);
 }
 
+static void initialize_nvs(void)
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
+
 void app_main(void)
 {
     delay(3000);
-    printf("\n");
-    printf("Running\n");
+    ESP_LOGI(TAG, "Running");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -66,7 +97,7 @@ void app_main(void)
            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "\n");
 
-    printf("silicon revision %d, \n", chip_info.revision);
+    ESP_LOGI(TAG, "silicon revision %d", chip_info.revision);
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
@@ -89,21 +120,32 @@ void app_main(void)
     myDisplay.displayText("Dada is The G.O.A.T", PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 
     if (xTaskCreate(flashLED, "flashLED", 1024 * 2, NULL, 10, &flashLED_TaskHandler) != pdPASS) {
-        printf("Failed to create task");
+        ESP_LOGE(TAG, "Failed to create task");
     }
 
-    String serial_input = "";
 
     while (1) {
         if (myDisplay.displayAnimate()) {
             myDisplay.displayReset();
         }
 
-        while (Serial.available() > 0) {
-            serial_input = Serial.readStringUntil('\n');
-            myDisplay.displayClear();
-            myDisplay.displayText(serial_input.c_str(), PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-        }
+        esp_console_repl_t *repl = NULL;
+        esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+        /* Prompt to be printed before each line.
+        * This can be customized, made dynamic, etc.
+        */
+        repl_config.prompt = PROMPT_STR ">";
+        repl_config.max_cmdline_length = CONFIG_CONSOLE_MAX_COMMAND_LINE_LENGTH;
+
+        initialize_nvs();
+
+        // while (Serial.available() > 0) {
+        //     serial_input = Serial.readStringUntil('\n');
+        //     myDisplay.displayClear();
+        //     myDisplay.displayText(serial_input.c_str(), PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+        // }
+
+        ESP_ERROR_CHECK(esp_console_start_repl(repl));
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
